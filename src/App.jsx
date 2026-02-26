9ffbf3357a2e37cfa11d2e4667b6e09a9164a982
KenMirwell <kenmirwell@gmail.com>
2026-02-26 14:23:55 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HRISHome from './HomeTemplate'
-import People from './PeopleTemplate'
-import AddEmployee from './AddEmployeeTemplate'
+import HRISHome from './templates/HomeTemplate'
+import AddEmployee from './templates/AddEmployeeTemplate'
+import PayrollPage from './templates/PayrollTemaplte'
+import People from './templates/PeopleTemplate'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <PayrollPage/>
     // <HRISHome/>
     // <People/>
-    <AddEmployee/>
+    // <AddEmployee/>
   )
 }
 
